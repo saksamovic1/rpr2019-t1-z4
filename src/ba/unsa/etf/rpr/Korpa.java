176ca4fb09d0608ab4a753dd2b2c90e145c8a648
Sandina <saksamovic1@etf.unsa.ba>
2020-11-06 16:17:33 +0100

    Initial commit

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] korpa = new Artikl[50] ;
+    private static int duzina = 0;
+    public Artikl[] getArtikli() {
+        return korpa;
+    }
+    public boolean dodajArtikl(Artikl a) {
+        if (duzina < 50) {
+            korpa[duzina]=a;
+            duzina++;
+            return true;
+        }
+        else return false;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl a=new Artikl("0",0,"0");
+        for (int i=0; i<duzina; i++) {
+            if (korpa[i].getKod().equals(kod)) {
+                a=korpa[i];
+                for (int j=i; j<duzina-1; j++) {
+                korpa[j]=korpa[j+1];
+            }
+                duzina--;
+                return a;
+            }
+        }
+        return null;
+    }
+    public int dajUkupnuCijenuArtikala() {
+        int ukupna_cijena=0;
+        for (int i=0; i<duzina; i++) {
+            ukupna_cijena=ukupna_cijena+korpa[i].getCijena();
+        }
+        return ukupna_cijena;
+    }
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private static Artikl[] supermarket = new Artikl[1000];
+    private static int duzina = 0;
+    public void dodajArtikl(Artikl a) {
+        if (duzina<1000) {
+            supermarket[duzina]=a;
+            duzina++;
+        } else System.out.println("Dostignut maksimalan broj artikala.");
+    }
+    public Artikl[] getArtikli() {
+        return supermarket;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl a=new Artikl("0",0,"0");
+
+        for (int i=0; i<duzina; i++) {
+            if (supermarket[i].getKod().equals(kod)) {
+                a = supermarket[i];
+                for (int j = i; j < duzina - 1; j++) {
+                    supermarket[j] = supermarket[j + 1];
+                }
+                duzina--;
+                return a;
+            }
+        }
+        return null;
+    }
 }
